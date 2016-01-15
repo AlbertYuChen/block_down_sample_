613f95e71cb7111157790239a0350c50393c4569
Albert Chen <chenyu610103@gmail.com>
2016-01-14 23:52:32 -0500

    switch user to yc576

--- main.cpp
+++ main.cpp
@@ -1,21 +1,27 @@
 #include <pthread.h>
+#include <stdio.h>
 
-pthread_spinlock_t slock;
+pthread_barrier_t b;
 
-void splock()
+void task(void* param)
 {
-    int i = 100;
-    while(i>0) {
-        pthread_spin_lock(&slock);
-        i--;
-        pthread_spin_unlock(&slock);
-    }
+    int id = (int)param;
+    printf("before the barrier %d\n", id);
+    pthread_barrier_wait(&b);
+    printf("after the barrier %d\n", id);
 }
 
 int main()
 {
-    pthread_spin_init(&slock, PTHREAD_PROCESS_PRIVATE);
-    splock();
-    pthread_spin_destroy(&slock);
+    int nThread = 5;
+    int i;
+
+    pthread_t thread[nThread];
+    pthread_barrier_init(&b, 0, nThread);
+    for(i = 0; i < nThread; i++)
+        pthread_create(&thread[i], 0, task, (void*)i);
+    for(i = 0; i < nThread; i++)
+        pthread_join(thread[i], 0);
+    pthread_barrier_destroy(&b);
     return 0;
 }
